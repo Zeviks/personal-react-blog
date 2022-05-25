298a5ae2f3197260019fa707db1b2bf52cafa86c
Natanael Pacheco <natepacheco716@gmail.com>
2022-05-25 01:07:02 -0500

    Completed Global Styles.

--- pages/index.js
+++ pages/index.js
@@ -17,12 +17,9 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
-      
-      <div>
-        {posts.map((post, index) => (
-          <PostCard post={post} key={post.title}/>
-          ))}
-      </div>
+        <div className="lg:col-span-8 col-span-1">
+          {posts.map((post, index) => <PostCard post={post} key={post.title}/> )}
+        </div>
         <div className="lg:col-span-4 col-span-1">
           <div className='lg:sticky relative top-8'>
             <PostWidget />
